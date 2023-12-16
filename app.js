0d0e97f881bed1ca2db7960145d358e9fc670834
basilsrh <basilsrh@gmail.com>
2023-12-17 03:06:42 +0400

    switch to puppeteer-core

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+  const browser = await puppeteer.launch({
+    executablePath: '/path/to/chromium',
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
   const page = await browser.newPage();
   await page.goto('https://example.com');
   console.log('Website title:', await page.title());
