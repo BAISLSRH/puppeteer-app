f8928c532a959aba59121a342ea1b8e28e7ab85c
basilsrh <basilsrh@gmail.com>
2023-12-17 03:46:19 +0400

    change app.js

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const puppeteer = require('puppeteer-core');
 
 (async () => {
   const browser = await puppeteer.launch({
-    executablePath: '/path/to/chromium',
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
-  });
+  executablePath: process.env.CHROMIUM_PATH || '/usr/bin/chromium', // Adjust the fallback path as needed
+  args: ['--no-sandbox', '--disable-setuid-sandbox']
+});
+
   const page = await browser.newPage();
   await page.goto('https://example.com');
   console.log('Website title:', await page.title());
