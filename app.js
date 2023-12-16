d8606a9bf4f730b4158b622b092d41ac62ce39b1
basilsrh <basilsrh@gmail.com>
2023-12-17 02:53:26 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+  const page = await browser.newPage();
+  await page.goto('https://example.com');
+  console.log('Website title:', await page.title());
+  await browser.close();
+})();
